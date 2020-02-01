ff93f51553c7fb16b1176bcfb33fd7902b6bd6f9
buzardh <buzardh@wwu.edu>
2020-02-01 22:31:28 +0000

    Update q5

--- q5.sql
+++ q5.sql
@@ -1,2 +1,2 @@
-# Display the first name, last name and rounded years worked of all employees sorted in a desceding order based on the rounded years
-
+SELECT first_name, last_name, ROUND(DATEDIFF(CURDATE(), hire_date)/365) AS years
+FROM employees ORDER BY years DESC;
