9a28dc64d2f24a0112cc340ce6e634cca7b6a45c
buzardh <buzardh@wwu.edu>
2020-02-01 22:30:47 +0000

    Update q4

--- q4.sql
+++ q4.sql
@@ -1,2 +1,5 @@
 # Display the first and last name of the employee that has a first name 'Elvis' and is the oldest employee
-
+SELECT first_name, last_name 
+FROM employees 
+WHERE first_name = 'Elvis'
+ORDER BY birth_date ASC LIMIT 1;
