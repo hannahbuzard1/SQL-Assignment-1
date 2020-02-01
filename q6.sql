da0d20eae94fe0eee66ade760d165dc4db96857c
buzardh <buzardh@wwu.edu>
2020-02-01 22:33:55 +0000

    Update q6

--- q6.sql
+++ q6.sql
@@ -1,2 +1,4 @@
 # Display the age of the oldest employee (as an int) in absolute years (assuming they are still working there)
-
+SELECT ROUND(DATEDIFF(CURDATE(),(SELECT birth_date FROM employees ORDER BY birth_date ASC LIMIT 1))/365)
+FROM dept_emp
+WHERE to_date > CURDATE() LIMIT 1
