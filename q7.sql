c8accd4922aea2ae065fec8c29a68a183f865863
buzardh <buzardh@wwu.edu>
2020-02-01 22:34:25 +0000

    Update q7

--- q7.sql
+++ q7.sql
@@ -1,2 +1,4 @@
 # Display the sex and counts of employees for each sex
-
+(SELECT gender, count(gender) FROM employees WHERE gender = "F")
+UNION
+(SELECT gender, count(gender) FROM employees WHERE gender = "M")
